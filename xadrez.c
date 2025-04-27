20613c58ba243bfc5b84db65a79ea7d15a1a7a47
Jc4292 <jcaesar7637@gmail.com>
2025-04-27 17:15:11 +0000

    added horse

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,7 @@
 
 int main() {
     unsigned int p_torre = 5,p_bispo = 5, p_rainha = 8;
+    unsigned int p_cavalo_x = 1, p_cavalo_y = 2;
 
     printf("torre:\n\n");
 
@@ -18,6 +19,15 @@ int main() {
     printf("\nrainha:\n\n");
 
     for(int i = 1; i <= p_rainha; i++){
+        printf("esquerda\n");
+    }
+
+    printf("\ncavalo:\n\n");
+    
+    for(int i = 1; i <= p_cavalo_x; i++){
+        for(int j = 1; j <= p_cavalo_y; j++){
+            printf("baixo\n");
+        }
         printf("esquerda\n");
     }
 
